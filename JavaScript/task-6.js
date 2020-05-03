cedd7943ead4c2d9b7535840a8081427c03e627b
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-05-03 11:29:54 +0300

    v2

--- JavaScript/task-6.js
+++ JavaScript/task-6.js
@@ -1,3 +1,26 @@
 let input;
 const numbers = [];
 let total = 0;
+
+while (true) {
+    input = prompt("Enter a number");
+
+    if (input === null) {
+        break;
+    }
+
+    input = Number(input);
+    const isNumber = Number.isNaN(input)
+
+    if (isNumber) {
+        alert('It is not a number, try again')
+        continue
+    }
+    numbers.push(input)
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i]
+}
+console.log(numbers)
+alert(`Total sum is ${total}`);
--- JavaScript/task-7.js
+++ JavaScript/task-7.js
@@ -1,21 +1,30 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = function (login) {
-    // твой код
+    if (login.length >= 4 & login.length <= 16) {
+        return true
+    } else {
+        return false
+    }
 };
 
 const isLoginUnique = function (allLogins, login) {
-    // твой код
+    return allLogins.includes(login)
 };
 
 const addLogin = function (allLogins, login) {
-    // твой код
+    if (isLoginValid(login) === false) {
+        return (`Ошибка! Логин должен быть от 4 до 16 символов`)
+    }
+    if (isLoginUnique(allLogins, login) === false) {
+        return ('Такой логин уже используется!')
+    }
+    allLogins.push(login)
+    return (`Логин успешно добавлен!`)
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+console.log(addLogin(logins, 'Ajax'));
+console.log(addLogin(logins, 'robotGoogles'));
+console.log(addLogin(logins, 'Zod'));
+console.log(addLogin(logins, 'jqueryisextremelyfast')); 
